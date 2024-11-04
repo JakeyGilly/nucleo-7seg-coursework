0f3a552c3faf5d0ffcf302a94e5969e4bea2471a
Jake Gillman <jakegillman3@gmail.com>
2024-11-04 14:47:49 +0000

    implemented custom names and counting the amount of times displayed

--- main.cpp
+++ main.cpp
@@ -40,8 +40,19 @@ int main() {
     // set the buttons to input with pull-up resistors
     DigitalIn btn1(D9, PullUp);
     DigitalIn btn2(D10, PullUp);
-    char fname[6] = "jake8";
-    char lname[6] = "gillm";
+
+    // set the first and last name as character arrays
+    char fname[6];
+    char lname[6];
+
+    // get the first and last name as input from the user
+    printf("Enter the first name: ");
+    scanf("%5s", fname);
+    printf("Enter the second name: ");
+    scanf("%5s", lname);
+
+    int count1 = 0; 
+    int count2 = 0;
     
     while (true) {
         // check if button 1 is pressed
@@ -50,12 +61,14 @@ int main() {
             for (int i = 0; i < 5; i++) {
                 display_char(fname[i]);
             }
+            printf("first name has been displayed %d times\n", ++count1);
         // check if button 2 is pressed
         } else if (!btn2) {
             // for each character in the last name, display it on the 7-segment display
             for (int i = 0; i < 5; i++) {
                 display_char(lname[i]);
             }
+            printf("last name has been displayed %d times\n", ++count2);
         }
     }
 }
