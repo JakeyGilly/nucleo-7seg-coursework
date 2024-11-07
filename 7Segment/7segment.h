ecc0eeb30c537a9f84f2896b4838120dd748c0be
Jake Gillman <jakegillman3@gmail.com>
2024-11-07 22:44:15 +0000

    tidy

--- 7Segment/7segment.h
+++ 7Segment/7segment.h
@@ -5,13 +5,7 @@ class SevenSegment {
     // delay for printing each character
     Kernel::Clock::duration_u32 _delay;
     // Pins for seven segment display
-    DigitalOut _A;
-    DigitalOut _B;
-    DigitalOut _C;
-    DigitalOut _D;
-    DigitalOut _E;
-    DigitalOut _F;
-    DigitalOut _G;
+    DigitalOut _A, _B, _C, _D, _E, _F, _G;
     // 7-segment display patterns for each character and digit
     int chars[26] = {250, 62, 26, 122, 158, 142, 188, 46, 136, 176, 174, 28, 170, 42, 58, 206, 230, 10, 180, 30, 56, 84, 86, 40, 118, 216};
     int digits[10] = {252, 96, 218, 242, 102, 182, 190, 224, 254, 246};
